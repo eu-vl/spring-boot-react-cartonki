bc30fbd8ee7d708810f91ea2f868a4ef6dda47b6
bayard7523 <49534036+bayard7523@users.noreply.github.com>
2021-04-21 17:19:31 +0300

    initial commit

---
+++ backend/src/main/java/com/backend/Program.java
@@ -0,0 +1,10 @@
+package com.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
